c11dceff1a4c962121db016a3764063475e5de96
Travis CI <github@greggman.com>
2017-04-25 05:33:55 +0000

    Deploy to GitHub Pages: f24ff5673ad9c4c9cf416a7defdc631ec839764b

---
+++ langdb.js
@@ -0,0 +1,13 @@
+window.langDB = {
+  "articles": {
+    "opengl-fundamentals.html": {
+      "en": true
+    }
+  },
+  "langs": {
+    "en": {
+      "lang": "en",
+      "language": "English"
+    }
+  }
+}
